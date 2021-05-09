d13dab1c4ebb46df6e741072be0104a449027fed
Simon Fisher <60694318+SimonF92@users.noreply.github.com>
2021-05-09 13:49:40 +0100

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,197 @@
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+import numpy as np
+%matplotlib inline  
+import warnings
+import matplotlib.cbook
+import matplotlib.cm as cm
+warnings.filterwarnings("ignore",category=matplotlib.cbook.mplDeprecation)
+from adjustText import adjust_text
+import pandas as pd
+from datetime import date
+
+
+
+
+def cast_to_map(simb_df):
+    
+    fig, ax = plt.subplots(figsize=(50,30))
+    
+    
+       
+    def create_map_centre():
+    
+        lons=simb_df['longitude'].values.tolist()
+        centre_lon=np.mean(lons)
+        lats=simb_df['latitude'].values.tolist()
+        centre_lat=np.mean(lats)
+        
+        return lons, lats, centre_lon, centre_lat
+    
+               
+    
+    def create_map_template():
+        
+        zoom=1
+    
+        m = Basemap(projection='stere',lat_0=centre_lat, lon_0=centre_lon,  lat_ts=centre_lat,resolution='l', width=2000000/zoom, height=1500000/zoom,ax=ax)
+        x_values, y_values = m(lons,lats)
+
+        m.fillcontinents(color='gray',lake_color='gray')
+        m.drawcoastlines()
+        m.drawparallels(np.arange(-80.,81.,20.))
+        m.drawmeridians(np.arange(-180.,181.,20.))
+        m.drawmapboundary(fill_color='white')
+       
+        return m, x_values, y_values 
+    
+    
+    def create_map_labels():
+        
+        
+        
+        
+        buoys_list=simb_df_full['buoy_id'].values.tolist()
+        buoys= list(set(buoys_list))
+
+
+        labels_df=[]
+
+        for buoy in buoys:
+            cut = simb_df_full[simb_df_full['buoy_id']==buoy]
+            deployment_date= cut.head(1)
+            current_date= cut.tail(1)
+            labels_df.append(deployment_date)
+            labels_df.append(current_date)
+
+        labels_df=pd.concat(labels_df)
+        labels_df
+
+
+
+
+        
+        date_labels=labels_df['date'].dt.date.tolist()
+        ice_thickness_labels=labels_df['ice_thickness'].values.tolist()
+        lons=labels_df['longitude'].values.tolist()
+        lats=labels_df['latitude'].values.tolist()
+        x_labels, y_labels=m(lons, lats)
+
+        
+        date_and_thickness_labels=[]
+    
+        for date,ice in zip(date_labels,ice_thickness_labels):
+            full_label=(str(date)+ ' ' + str(round(ice,2)) + 'm')
+        
+            date_and_thickness_labels.append(full_label)
+            
+        
+        
+        
+        
+    
+    
+        return x_labels, y_labels, date_and_thickness_labels
+ 
+
+
+    def produce_final_figure():
+        
+        colours=simb_df['air_temp'].values.tolist()
+    
+    
+    
+    
+    
+
+        final_figure=m.scatter(x_values, y_values,100,c=colours,ax=ax,cmap='jet')
+    
+        final_figure_labels=[]
+
+        for (x,y, label) in zip(x_labels, y_labels, date_and_thickness_labels):
+
+            t=ax.text(x, y, label,color='black', ha='center', size=30,
+             bbox=dict(boxstyle="round", facecolor='lightgrey', alpha=0.5 ))
+            
+            final_figure_labels.append(t)
+
+
+        adjust_text(final_figure_labels, ax=ax, precision=0.001,
+        expand_text=(2.01, 2.05), expand_points=(2.01, 2.05),
+        force_text=(0.01, 0.25), force_points=(0.01, 0.25),
+        arrowprops=dict(arrowstyle='-', color='gray', lw=5, alpha=.8),ha='right')
+    
+    
+        cbar=plt.colorbar(final_figure,shrink=0.5,ax=ax)
+        cbar.set_label("Surface Temperature (degC)",size=30)
+        cbar.ax.tick_params(labelsize=25) 
+        
+        return fig
+    
+    
+
+
+    lons, lats, centre_lon, centre_lat= create_map_centre()
+    m, x_values, y_values = create_map_template()
+    x_labels, y_labels, date_and_thickness_labels = create_map_labels()
+    fig= produce_final_figure()
+
+    plt.show()
+
+    
+    
+    
+
+
+def fetch_and_map_data():
+    #url='https://app.cryosphereinnovation.com/sbd_data/SIMB3_443910.csv'
+    #url='https://app.cryosphereinnovation.com/sbd_data/SIMB3_859790.csv'
+    #url='https://app.cryosphereinnovation.com/sbd_data/SIMB3_441910.csv'
+    
+    urls=['https://app.cryosphereinnovation.com/sbd_data/SIMB3_443910.csv',
+         'https://app.cryosphereinnovation.com/sbd_data/SIMB3_859790.csv',
+         'https://app.cryosphereinnovation.com/sbd_data/SIMB3_441910.csv']
+    
+    
+    frames=[]
+    
+    for url in urls:
+        
+        
+        print(url)
+        try:
+
+            simb_df = pd.read_csv(url) 
+            buoy_id= url.split('/')[-1].rstrip('.csv')
+            simb_df=simb_df[simb_df['wdt_counter']>2]
+            simb_df['buoy_id']=buoy_id
+
+        except invalid_link:
+            print('Invalid link, please check link address (right click on "download csv" and copy link')
+
+        initial_snow_depth=0.2
+        initial_surface_distance=simb_df['surface_distance'].values.tolist()[0]
+        #distance_between_rangefinders=[0, 3378]
+
+        height_above_ice = initial_surface_distance + initial_snow_depth
+
+
+        simb_df['snow_depth']=height_above_ice - simb_df['surface_distance'] 
+        #4.05 is sounder distance
+        simb_df['ice_thickness']= 4.05- height_above_ice - simb_df['bottom_distance']
+
+
+        simb_df['time_stamp']=simb_df['time_stamp'].round(0)
+        simb_df['date']=pd.TimedeltaIndex(simb_df['time_stamp'],unit='d')+pd.to_datetime('1900-01-01')
+        
+        frames.append(simb_df)
+        #print(frames)
+        
+        
+    simb_df_full=pd.concat(frames,ignore_index=True)
+    
+     #return simb_df_full
+    
+    cast_to_map(simb_df_full)
+
+fetch_and_map_data()
